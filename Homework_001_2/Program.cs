c03e83f781788ff0832cac41a49e9d511343c87e
Ilya <Semenoffqwe@gmail.com>
2023-09-24 23:40:35 +0300

    Выполнили 2 задачу

--- Homework_001_2/Program.cs
+++ Homework_001_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a>b && a>c){
+    max = a;
+    Console.WriteLine("max = " + max);
+}
+if(b>a && b>c){
+    max = b;
+    Console.WriteLine("max = " + max);
+    
+}
+if(c>b && c>a){
+    max = c;
+    Console.WriteLine("max = " + max);
+    
+}
