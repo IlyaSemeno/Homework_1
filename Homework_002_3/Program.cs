8ba6db92d82114f4b14b0c5b2759aa62504b8b8e
Ilya <Semenoffqwe@gmail.com>
2023-09-25 00:02:58 +0300

    Выполнили 3 задачу 2 домашнего задания.

--- Homework_002_3/Program.cs
+++ Homework_002_3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==6 || number == 7 && number <=7 && number >=1){
+    Console.WriteLine("Да, это выходной");
+}
+else{
+    Console.WriteLine("Нет, это будни!!");
+}
