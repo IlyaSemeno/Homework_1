f02354552e8b3b3defd7d59d477442ab6222b590
Ilya <Semenoffqwe@gmail.com>
2023-09-24 23:51:07 +0300

    Выполнили 4 задачу

--- Homework_001_4/Program.cs
+++ Homework_001_4/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Впишите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 2; i<=N; i+=2){
+    Console.WriteLine(i);
+}
