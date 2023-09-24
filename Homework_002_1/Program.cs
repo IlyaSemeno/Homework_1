a7429681e49f486ac6e5d756bbb90cad2ba30121
Ilya <Semenoffqwe@gmail.com>
2023-09-24 23:56:52 +0300

    Выполнили 1 задачу 2 домашнего задания.

--- Homework_002_1/Program.cs
+++ Homework_002_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>=100 && number<=999){
+    int secondnumber = (number/10)%10;
+    Console.WriteLine("Вторая цифра: " + secondnumber);
+}
+else{
+    Console.WriteLine("Это не трехзначное число.");
+}
